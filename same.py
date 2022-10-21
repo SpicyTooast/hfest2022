e035beea903dd99f007835ef644746acd40322be
Spicy Toast <116366898+SpicyTooast@users.noreply.github.com>
2022-10-21 21:54:35 +0530

    Added

--- same.py
+++ same.py
@@ -3,3 +3,5 @@ print("Arey ye hum dono ke dummy repo ka readme same keise he!")
 print("Kahi eisa to nhi ki tumne uski copy ki he")
 
 print("Bilkul b nahi or  maybe or im confused")
+
+print("Why not, bilkul ho sakta he")
